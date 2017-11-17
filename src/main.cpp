4c85c440a9aef677b02f969659dad96c52cba35d
Peter Lewis <peter@werl.me>
2017-11-17 13:10:15 -0700

    add the model to render

--- src/main.cpp
+++ src/main.cpp
@@ -61,6 +61,8 @@ int main(void) {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
+        mesh->render();
+
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
